d422e78b2bd0ce9f7439810fd643a226dfd630bc
Vackyton <vackytonplays@gmail.com>
2021-09-01 14:31:13 -0400

    Update If_Statements!.py

--- If_Statements!.py
+++ If_Statements!.py
@@ -3,18 +3,18 @@
 #   Made by Vackyton
 Password1 = input(" Enter the password to access files : ")
 if Password1 == "Wackyness":
-    print("Vackyton likes Computers And Coding!")
-#   Remember to make sure there is a space between the start of the line or it will not work
-#   This statement makes sure the password is right!Make sure it is lowercase or it will need to
-#   be perfect caps for ex:wackyness will return "Lolno" unless it is "Wackyness"
+    print("Dwij likes Computers And Coding!")
+    #   Remember to make sure there is a space between the start of the line or it will not work
+    #  This statement makes sure the password is right!Make sure it is lowercase or it will need to
+    #   be perfect caps for ex:wackyness will return "Lol no" unless it is "Wackyness"
 elif Password1 == "NoWack":
     print("Fine you got me")
-    #elif statements mean else if and can be used forever!
+    #   elif statements mean else if and can be used forever!
+    #   elif statements can be used to make more passwords
 elif Password1 == "FREEKEY":
     print("REEEEEEEEEE HOW DID U KNOW")
 else:
     print("Wrong password access denied :)")
     
-#   The else statement does what it means. If the password is not "Wackyness" It will run the else statement,
-#   you can make a another password
-#   So know you know how to make a password!
+    #   The else statement does what it means. If the password is not "Wackyness" It will run the else statement. and there can be only 1.
+    #   So know you know how to make a password!
