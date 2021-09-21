78ad534dd303e59a6cc133580413c8215b04e880
Vackyton AKA Pure Idiot Wants to die =1 <vackytonplays@gmail.com>
2021-09-21 09:32:29 -0400

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -4,7 +4,7 @@ num1 = input("First Number: ")
 num2 = input("Second Number: ")
 
 if type == "/":
-    print(num1 / num2)
+    print(float(num1) / float(num2))
 
 if type == "*":
     print(float(num1) * float(num2))
