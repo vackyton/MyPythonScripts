3a600d382604a551adb196eca3598e3bcb6d7491
Vackyton AKA Pure Idiot Wants to die =1 <vackytonplays@gmail.com>
2021-09-22 10:53:21 -0400

    Create SimplePasswordScript.py

---
+++ files/SimplePasswordScript.py
@@ -0,0 +1,20 @@
+#Password Script
+
+    #First/Only Password
+Password = input("Enter your password : ")
+if Password == "PASSKey":
+    print("Thing that it should say")
+    
+    #this codestart to codeend is not to be copied if you only want 1 password.
+    
+    #codestart
+elif Password == "If You want more password copy this codestart to codeend":
+    print("Thing it should say")
+#codeend
+
+    #The piece of Script that says wrong password codestart
+else:
+    time.sleep(10) #Torture the soul with a wait command
+    print ("Wrong Password")
+
+    #codeend
